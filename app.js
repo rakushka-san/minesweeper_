10e1942c6da726fd7d4ad9eda252481fd23eb803
rakushka-san <vladislavmohov@mail.ru>
2023-10-18 00:27:11 +0300

    Отвязка открытия клетки от css-классов

--- app.js
+++ app.js
@@ -105,21 +105,14 @@ document.addEventListener('DOMContentLoaded', () => {
 	function click(square) {
 		let currentId = square.id
 		if (isGameOver) return
-		if (
-			square.classList.contains('checked') ||
-			square.classList.contains('flag')
-		)
-			return
-		if (square.classList.contains('bomb')) {
+		if (gameArray[square.id].isChecked || gameArray[square.id].isFlagged) return
+		if (!gameArray[square.id].isValid) {
 			gameOver(square)
 		} else {
-			let total = square.getAttribute('data')
+			let total = gameArray[square.id].bombsAround
 			if (total != 0) {
 				square.classList.add('checked')
-				if (total == 1) square.classList.add('one')
-				if (total == 2) square.classList.add('two')
-				if (total == 3) square.classList.add('three')
-				if (total == 4) square.classList.add('four')
+				square.classList.add(`cell-${total}`)
 				square.innerHTML = total
 				return
 			}
